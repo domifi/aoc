8a1450c4e0b385cb9a4c2751b8dd4a4d808f1bb3
domifi <domifi@outlook.com>
2022-12-06 20:55:10 +0100

    day04a done

--- 2022/04/a.rs
+++ 2022/04/a.rs
@@ -12,10 +12,20 @@ fn main() {
         .map(|l| line2pair(l.unwrap()))
         .collect();
 
-    println!("{:?}", pairs)
+    let sum = pairs
+        .iter()
+        .map(|pair| contains(&pair.0, &pair.1) || contains(&pair.1, &pair.0))
+        .filter(|b| *b)
+        .count();
+
+    println!("{}", sum)
 }
 
 fn line2pair(l: String) -> (Section, Section) {
     let numbers: Vec<u64> = l.split(&[',', '-'][..]).map(|e| e.parse().unwrap()).collect();
     (Section(numbers[0], numbers[1]), Section(numbers[2], numbers[3]))
+}
+
+fn contains(outer: &Section, inner: &Section) -> bool {
+    outer.0 <= inner.0 && inner.1 <= outer.1
 }
