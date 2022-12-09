ae952aacdd16a1c973695095a44ecd327cc8ace8
domifi <domifi@outlook.com>
2022-12-09 13:41:55 +0100

    I hate this task

--- 2022/07/a.rs
+++ 2022/07/a.rs
@@ -2,15 +2,51 @@ use std::io;
 use std::io::BufRead;
 
 #[derive(Debug)]
-struct File<'a> {
-    name: &'a str,
+struct File {
+    name: String,
     size: u64,
+}
+
+impl File {
+    fn new(name: &str, size: u64) -> File {
+        File {name: name.to_owned(), size}
+    }
 }
 
 #[derive(Debug)]
 struct Directory<'a> {
     name: &'a str,
-    contens: &'a Vec<Result<File<'a>, Directory<'a>>>,
+    files: Vec<File>,
+    directories: Vec<Directory<'a>>,
+}
+
+impl Directory<'_> {
+    fn new(name: &str) -> Directory {
+        Directory {
+            name,
+            files: Vec::new(),
+            directories: Vec::new(),
+        }
+    }
+
+}
+
+trait Inode {
+    fn size(&self) -> u64;
+}
+
+impl Inode for Directory<'_> {
+    fn size(&self) -> u64 {
+        self.files.iter().map(|f| f.size()).sum::<u64>()
+        +
+        self.directories.iter().map(|f| f.size()).sum::<u64>()
+    }
+}
+
+impl Inode for File {
+    fn size(&self) -> u64 {
+        self.size
+    }
 }
 
 fn main() {
@@ -19,12 +55,17 @@ fn main() {
         .lines()
         .map(|l| l.unwrap());
 
-    let marker = input
-        .as_bytes()
-        .windows(14)
-        .zip(14..)
-        .find(|sequence| !has_dups(sequence.0))
-        .expect("no subsequence without duplicates found");
+    let mut filesystem = Directory::new("/");
 
-    println!("{:?}", marker.1)
+    // for line in input {
+    //     if line.starts_with("$ cd") {
+    //         let name = line.split_whitespace().last().unwrap();
+    //         if name == "/" {
+    //             continue;
+    //         }
+    //     }
+    // }
+
+
+    println!("{:?}", input.collect::<Vec<_>>())
 }
